09cf6bcd9b8882aa89d1699a09165dd56f783258
pb-coding <pb@philippbeckmann.de>
2023-03-20 17:17:45 +0100

    adds selectable parameter charts

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ParameterCard from './components/ParameterCard'
 import { badgeType } from './utils/utils'
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 function App() {
+  
+  const drivingExperience = {
+    name: 'Fahrerfahrung',
+    data: [{name: '< 1 Jahr', uv: 10}, {name: '1 - 3 Jahre', uv: 40}, {name: '3 - 10 Jahre', uv: 70}, {name: '30 - 60 Jahre', uv: 120}, {name: '> 60 Jahre', uv: 30}],
+  }
 
-  const data = [{name: '< 1 Jahr', uv: 10}, {name: '1 - 3 Jahre', uv: 40}, {name: '3 - 10 Jahre', uv: 70}, {name: '30 - 60 Jahre', uv: 120}, {name: '> 60 Jahre', uv: 30}];
+  const numberOfAccidents = {
+    name: "Unfallanzahl",
+    data: [{name: 'Unfallfrei', uv: 100}, {name: '1', uv: 80}, {name: '2', uv: 30}, {name: '3', uv: 20}, {name: '> 4', uv: 10}],
+  }
 
-  const lineChart = (
-    <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-      <XAxis dataKey="name" />
-      <YAxis />
-    </LineChart>
-  );
+  const [selectedParameter, setSelectedParameter] = useState(drivingExperience)
 
   return (
     <div className="App">
@@ -31,15 +32,15 @@ function App() {
             vom Typ "input-influence based".
 
             Tim ist ein Kunde des Versicherungsunternehmens. Er ist:
-            <ul>
-              <li>35 Jahre alt</li>
-              <li>fährt seit 17 Jahren Auto</li>
-              <li>War einmal in einen Unfall verwickelt, der nicht seine Schuld war</li>
-              <li>Fährt durchschnittlich 800 Meilen pro Monat</li>
-              <li>Überschreitet die Geschwindigkeitsbegrenzung im Durchschnitt einmal alle zwei Monate</li>
-              <li>20 % von Tims Fahrten finden nachts statt.</li>
-            </ul>
           </p>
+          <ul className="space-y-1 list-disc list-inside dark:text-gray-400">
+            <li>35 Jahre alt</li>
+            <li>fährt seit 17 Jahren Auto</li>
+            <li>War einmal in einen Unfall verwickelt, der nicht seine Schuld war</li>
+            <li>Fährt durchschnittlich 800 Meilen pro Monat</li>
+            <li>Überschreitet die Geschwindigkeitsbegrenzung im Durchschnitt einmal alle zwei Monate</li>
+            <li>20 % von Tims Fahrten finden nachts statt.</li>
+          </ul>
 
           <h5 className="mt-8 mb-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             Parameter
@@ -51,6 +52,8 @@ function App() {
               
               <ParameterCard 
                 link="#"
+                onClick={() => setSelectedParameter(drivingExperience)}
+                active={selectedParameter.name === drivingExperience.name}
                 heading="Fahrerfahrung" 
                 badgeType={badgeType.green} 
                 badgeText="++" 
@@ -59,6 +62,8 @@ function App() {
 
               <ParameterCard
                 link="#"
+                onClick={() => setSelectedParameter(numberOfAccidents)}
+                active={selectedParameter.name === numberOfAccidents.name}
                 heading="Anzahl vergangener Verkehrsunfälle"
                 badgeType={badgeType.yellow}
                 badgeText="+"
@@ -100,7 +105,12 @@ function App() {
 
             <div id="chart" className="col-span-7">
               
-              {lineChart}
+            <LineChart width={600} height={300} data={selectedParameter.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+              <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+              <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+              <XAxis dataKey="name" />
+              <YAxis />
+            </LineChart>
 
             </div>
 
--- src/components/ParameterCard.jsx
+++ src/components/ParameterCard.jsx
@@ -1,6 +1,6 @@
-const ParameterCard = ({ link, heading, badgeType, badgeText, body }) => {
+const ParameterCard = ({ link, onClick, active, heading, badgeType, badgeText, body }) => {
     return (
-        <a href={link} className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <a href={link} onClick={onClick} className={`block max-w-sm p-6 ${active?'bg-gray-200' : 'bg-white'} border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700`}>
             <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">{heading}</h5>
             <span className={badgeType}>{badgeText}</span>
             <p className="font-normal text-gray-700 dark:text-gray-400">{body}</p>
